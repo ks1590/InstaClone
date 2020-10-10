8b450a36ab1b17906d42045be7b425268580c219
kensuke suzuki <35k.0101@gmail.com>
2020-10-11 02:40:36 +0900

    コントローラー修正

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+  # before_action :authenticate_user!, except: [:index]
+  def index
+    @users = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path
+    end
+  end
+
+  def account
+    @user = User.new(user_params)
+  end
+
+  def confirm
+  end
+
+  def update
+    @user = current_user.users.build(user_params)
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :password,
+                                :password_confirmation, :image, :image_cache)
+  end
+end
